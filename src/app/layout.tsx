d7ddae68780929d98a4801aec5a93028b04a57e0
Chase Albritton <59696546+stunnas@users.noreply.github.com>
2024-04-30 13:53:19 -0400

    logo update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-        <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <link
+          rel="icon"
+          href="/caa.png"
+          sizes="any"
+        />
+      </body>
     </html>
   );
 }
